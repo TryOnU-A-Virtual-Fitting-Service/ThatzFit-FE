f1a6cdf1988634b7e1fe70c0f9445727f0e59b13
lee0jae330 <xingxing2001@cau.ac.kr>
2025-07-18 23:31:26 +0900

    feat: eslint 규칙 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,71 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+import stylisticJs from '@stylistic/eslint-plugin';
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules', '.gemini', '.git', '.vscode']),
+  stylisticJs.configs.customize({
+    arrowParens: true,
+    indent: 2,
+    semi: true,
+    commaDangle: 'always-multiline',
+    quotes: 'single',
+  }),
   {
+    plugins: {
+      '@stylistic': stylisticJs,
+    },
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      tseslint.configs.strictTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
+    rules: {
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/arrow-parens': ['error'],
+      '@stylistic/quotes': [
+        'error',
+        'single',
+        {
+          allowTemplateLiterals: true,
+        },
+      ],
+      '@stylistic/jsx-quotes': ['error'],
+      '@stylistic/semi': ['error'],
+      '@stylistic/max-len': ['error', { code: 80, tabWidth: 2 }],
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/arrow-parens': ['error', 'always'],
+      '@stylistic/keyword-spacing': ['error'],
+      '@stylistic/space-before-blocks': ['error'],
+      '@stylistic/space-infix-ops': ['error'],
+      '@stylistic/member-delimiter-style': [
+        'error',
+        {
+          multiline: {
+            delimiter: 'comma',
+            requireLast: true,
+          },
+          singleline: {
+            delimiter: 'comma',
+            requireLast: false,
+          },
+        },
+      ],
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
-])
+]);
