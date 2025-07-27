1d510258ab5562594e3552179f86c9c69e9a946c
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2025-07-27 21:24:18 +0900

    feat: fsd 아키텍처 기본 구조 추가 (#7)

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,18 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `index.[hash].js`,
+        chunkFileNames: `index-vendor.[hash].js`,
+        assetFileNames: `[name].[hash].[ext]`,
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'index-vendor';
+          }
+        }
+      }
+    }
+  }
 })
