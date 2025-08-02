d496259ad951db544354162ff2458255e1b4ce41
lee0jae330 <xingxing2001@cau.ac.kr>
2025-08-02 14:32:15 +0900

    feat: 타입 별칭 vite plugin 설치

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(), tsconfigPaths()],
     build: {
       rollupOptions: {
         input: resolve(__dirname, 'src/Apps/main.tsx'),
@@ -25,6 +26,9 @@ export default defineConfig(() => {
         }
       },
       emptyOutDir: true,
+    },
+    server: {
+      allowedHosts: ['http://localhost:8080'],
     }
   };
 })
