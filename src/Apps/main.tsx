90954772264e10f0bdaf17bd85a8a5ebd3fe4b38
lee0jae330 <xingxign2001@cau.ac.kr>
2025-08-02 17:34:31 +0900

    chore: iframeDocument 타입 체크 및 if문 수정

--- src/Apps/main.tsx
+++ src/Apps/main.tsx
@@ -6,22 +6,22 @@ import { App } from './App.tsx';
 import './index.css';
 
 if (import.meta.env.DEV) {
-  const iframe = document.getElementById('thatzfit-iframe') as HTMLIFrameElement;
-  if (iframe) {
-    createRoot(iframe.contentWindow!.document.getElementById('thatzfit-root')!).render(
+  const iframe = document.getElementById(
+    'thatzfit-iframe',
+  ) as HTMLIFrameElement;
+  const iframeDocument =
+    iframe.contentWindow?.document || iframe.contentDocument;
+  if (iframe && iframeDocument) {
+    createRoot(iframeDocument.getElementById('thatzfit-root')!).render(
       <StrictMode>
         <App />
       </StrictMode>,
     );
   }
-}
-
-if (import.meta.env.PROD) {
+} else {
   createRoot(document.getElementById('thatzfit-root')!).render(
     <StrictMode>
       <App />
     </StrictMode>,
   );
-  
 }
-
