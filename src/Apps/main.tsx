b0c962b88177d184e4dd36177927cec5cb22136b
lee0jae330 <xingxign2001@cau.ac.kr>
2025-08-02 16:45:19 +0900

    feat: dev 모드로 프로젝트 실행 시 index.html 내부에 있는 iframe 내부에 렌더링되도록 분기처리 추가

--- src/Apps/main.tsx
+++ src/Apps/main.tsx
@@ -5,9 +5,23 @@ import { App } from './App.tsx';
 
 import './index.css';
 
+if (import.meta.env.DEV) {
+  const iframe = document.getElementById('thatzfit-iframe') as HTMLIFrameElement;
+  if (iframe) {
+    createRoot(iframe.contentWindow!.document.getElementById('thatzfit-root')!).render(
+      <StrictMode>
+        <App />
+      </StrictMode>,
+    );
+  }
+}
 
-createRoot(document.getElementById('thatzfit-root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+if (import.meta.env.PROD) {
+  createRoot(document.getElementById('thatzfit-root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+  
+}
+
