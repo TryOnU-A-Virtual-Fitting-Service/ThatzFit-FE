de8928d7bf30c49e7cd037842e2fffee80cb647c
lee0jae330 <xingxing2001@cau.ac.kr>
2025-08-03 02:58:39 +0900

    feat: plugin 비활성화 버튼 추가

---
+++ src/Features/PluginEntry/Ui/PluginDeactivateButton/PluginDeactivateButton.tsx
@@ -0,0 +1,19 @@
+import { X } from 'lucide-react';
+
+import { Button } from '@/Shared';
+
+interface PluginDeactivateButtonProps {
+  className?: string;
+  onClick: () => void;
+}
+
+export const PluginDeactivateButton = ({
+  className,
+  onClick,
+}: PluginDeactivateButtonProps) => {
+  return (
+    <Button className={className} size='icon' onClick={onClick}>
+      <X />
+    </Button>
+  );
+};
