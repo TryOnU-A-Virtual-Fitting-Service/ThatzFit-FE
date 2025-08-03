d6455828cd1d0eebbc313d56b9f5df1e040c3e17
lee0jae330 <xingxign2001@cau.ac.kr>
2025-08-03 19:46:58 +0900

    feat: iframe css 클래스 생성 로직 추가

--- src/Apps/Model/initializeThatzfitStyle.ts
+++ src/Apps/Model/initializeThatzfitStyle.ts
@@ -1,6 +1,10 @@
 export const initializeThatzfitStyle = () => {
   document.styleSheets[0].insertRule(
-    '.thatzfit-desktop { position: fixed; bottom: 90px; right: 24px; width: 15rem; height: 570px; border-radius: 12px; box-shadow: 0 0 41.711px 0 rgba(0, 0, 0, 0.15); }',
+    '.thatzfit-desktop { position: fixed; bottom: 90px; right: 24px; width: 15rem; height: 570px; background-color: transparent; border-radius: 0.75rem; box-shadow: 0 0 41.711px 0 rgba(0, 0, 0, 0.15); }',
+  );
+
+  document.styleSheets[0].insertRule(
+    '#thatzfit-iframe { width: 100% !important; height: 100% !important; background-color: transparent !important; border-radius: 0.75rem !important; }',
   );
 
   document.styleSheets[0].insertRule(
