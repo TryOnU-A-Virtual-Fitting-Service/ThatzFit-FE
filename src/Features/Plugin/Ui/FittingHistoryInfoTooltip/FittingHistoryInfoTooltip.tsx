da134de5cbcc819364c612d08d938c236606d88d
lee0jae330 <xingxing2001@cau.ac.kr>
2025-08-07 00:31:32 +0900

    feat: 피팅 히스토리 설명 툴팁 container 지정

--- src/Features/Plugin/Ui/FittingHistoryInfoTooltip/FittingHistoryInfoTooltip.tsx
+++ src/Features/Plugin/Ui/FittingHistoryInfoTooltip/FittingHistoryInfoTooltip.tsx
@@ -1,4 +1,7 @@
+import { type MouseEvent, useState } from 'react';
 import { Info } from 'lucide-react';
+
+import { usePluginStore } from '@/Entities/Plugin';
 
 import {
   Tooltip,
@@ -7,12 +10,33 @@ import {
 } from '@/Shared/Components/Ui';
 
 export const FittingHistoryInfoTooltip = () => {
+  const pluginIframe = usePluginStore((state) => state.pluginIframe);
+  const iframeDocument = pluginIframe?.contentDocument;
+  const rootDiv = iframeDocument?.body;
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleMouseEnter = (e: MouseEvent<HTMLSpanElement>) => {
+    e.stopPropagation();
+    setIsOpen(true);
+  };
+
+  const handleMouseLeave = (e: MouseEvent<HTMLSpanElement>) => {
+    e.stopPropagation();
+    setIsOpen(false);
+  };
+
   return (
-    <Tooltip>
-      <TooltipTrigger>
-        <Info className='text-grey-07 size-3' />
+    <Tooltip open={isOpen} delayDuration={0}>
+      <TooltipTrigger asChild>
+        <span onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+          <Info className='text-grey-07 size-3.5' />
+        </span>
       </TooltipTrigger>
-      <TooltipContent>
+      <TooltipContent
+        container={rootDiv}
+        className='bg-light-blue fill-light-blue'
+      >
         <span className='text-body2-medium text-grey-03'>
           최대 20개까지 저장됩니다
         </span>
