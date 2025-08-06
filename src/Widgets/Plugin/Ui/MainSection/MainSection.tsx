13e901876063b059176090d46ab3b163ec9febc7
lee0jae330 <xingxing2001@cau.ac.kr>
2025-08-07 00:00:30 +0900

    feat: 피팅 히스토리 관련 section 추가

--- src/Widgets/Plugin/Ui/MainSection/MainSection.tsx
+++ src/Widgets/Plugin/Ui/MainSection/MainSection.tsx
@@ -1,9 +1,14 @@
+import { FittingButton } from '@/Features/Plugin';
+
+import { FittingHistorySection } from '../FittingHistorySection';
 import { FittingModelView } from '../FittingModelView';
 
 export const MainSection = () => {
   return (
-    <section className='w-full px-4'>
+    <section className='flex w-full flex-col px-4'>
       <FittingModelView />
+      <FittingHistorySection />
+      <FittingButton />
     </section>
   );
 };
