81f3350945bf33b04105e215d5bc4f0f0c68595a
lee0jae330 <xingxing2001@cau.ac.kr>
2025-08-10 02:03:07 +0900

    chore: hover관련 css 스타일 조정

--- src/Features/Plugin/Ui/FittingButton/FittingButton.tsx
+++ src/Features/Plugin/Ui/FittingButton/FittingButton.tsx
@@ -7,7 +7,7 @@ interface FittingButtonProps {
 export const FittingButton = ({ onClick }: FittingButtonProps) => {
   return (
     <Button
-      className='bg-grey-02 text-body1 h-8 w-full cursor-pointer rounded-md text-white'
+      className='bg-grey-02 text-body1 hover:text-grey-07 h-8 w-full cursor-pointer rounded-md text-white hover:bg-black'
       onClick={onClick}
     >
       입어보기
