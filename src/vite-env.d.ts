9579cf054ece837e2063f9b50ede96c7eef55c3e
lee0jae330 <xingxing2001@cau.ac.kr>
2025-08-12 00:49:18 +0900

    feat: env 타입 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,8 @@
 /// <reference types="vite/client" />
 
 /// <reference types="vite-plugin-svgr/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_DEV_SERVER: string;
+  readonly VITE_PROD_SERVER: string;
+}
