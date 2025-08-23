3261d6eec49b9c57fd1e1426bde8611ecc26b303
lee0jae330 <xingxign2001@cau.ac.kr>
2025-08-23 19:02:43 +0900

    feat: 피팅 페이지 로드 시 피팅 모델 전역 상태 초기화

--- src/Entities/User/index.ts
+++ src/Entities/User/index.ts
@@ -1,2 +1,2 @@
-export { postUserInfo } from './Api';
+export { postUserInfo, userQueries, userQueryKeys } from './Api';
 export { getUserToken } from './Model';
--- src/Pages/Plugin/Ui/FittingPage/FittingPage.tsx
+++ src/Pages/Plugin/Ui/FittingPage/FittingPage.tsx
@@ -1,12 +1,41 @@
+import { useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
+
 import {
   CompanyInfoSection,
   FooterSection,
   MainSection,
 } from '@/Widgets/Plugin';
 
+import { useFittingModelStore } from '@/Entities/FittingModel';
+import { userQueries } from '@/Entities/User';
+
 import { PluginLayout } from '../PluginLayout';
 
 export const FittingPage = () => {
+  const {
+    data: userInfo,
+    isLoading,
+    isSuccess,
+  } = useQuery({
+    ...userQueries.userInfo(),
+    select: (response) => response.data,
+  });
+
+  const setDefaultModels = useFittingModelStore(
+    (state) => state.setDefaultModels,
+  );
+
+  useEffect(() => {
+    if (isSuccess && userInfo) {
+      setDefaultModels(userInfo?.defaultModels ?? []);
+    }
+  }, [isSuccess, setDefaultModels, userInfo]);
+
+  if (isLoading) {
+    return null;
+  }
+
   return (
     <PluginLayout>
       <PluginLayout.Header>
