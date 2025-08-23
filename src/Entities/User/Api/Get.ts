0f594dbc55ab31565e27bfc2c806506cf51f3a46
lee0jae330 <xingxing2001@cau.ac.kr>
2025-08-24 01:10:01 +0900

    chore: user 관련 엔드포인트 수정

--- src/Entities/User/Api/Get.ts
+++ src/Entities/User/Api/Get.ts
@@ -3,5 +3,5 @@ import { get } from '@/Shared/Model';
 import type { GetUserInfoResponseDto } from '../Type';
 
 export const getUserInfo = async () => {
-  return get<GetUserInfoResponseDto>('/api/users/me').then((res) => res.json());
+  return get<GetUserInfoResponseDto>('/api/user/me').then((res) => res.json());
 };
--- src/Entities/User/Api/Post.ts
+++ src/Entities/User/Api/Post.ts
@@ -3,7 +3,7 @@ import { post } from '@/Shared/Model';
 import type { PostUserInfoRequestDto, PostUserInfoResponseDto } from '../Type';
 
 export const postUserInfo = async (dto: PostUserInfoRequestDto) => {
-  return post<PostUserInfoResponseDto>(`/api/users/init`, {
+  return post<PostUserInfoResponseDto>(`/api/user/init`, {
     body: JSON.stringify(dto),
   }).then((res) => res.json());
 };
