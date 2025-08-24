9eab2d31e328342a33e651eff13b856f65c1f1f6
lee0jae330 <xingxing2001@cau.ac.kr>
2025-08-24 21:29:41 +0900

    feat: 피팅 페이지 모델 뷰 전역 상태 연동 및 props 수정

--- src/Widgets/FittingModel/Ui/FittingModelView/FittingModelView.tsx
+++ src/Widgets/FittingModel/Ui/FittingModelView/FittingModelView.tsx
@@ -1,17 +1,24 @@
 import {
   FittingModelActionDialog,
   ModelZoomDialog,
-} from '@/Features/FittingModel';
+} from '@/Widgets/FittingModel';
 
-import { FittingModelImage } from '@/Entities/FittingModel';
+import {
+  FittingModelImage,
+  useFittingModelStore,
+} from '@/Entities/FittingModel';
 
 export const FittingModelView = () => {
+  const currentFittingModel = useFittingModelStore(
+    (state) => state.currentFittingModel,
+  );
+
   return (
     <div className='bg-grey-08 relative flex h-[18.75rem] w-full justify-center rounded-[0.375rem] p-[0.5625rem] select-none'>
       <FittingModelActionDialog dialogTriggerClassName='absolute top-2 left-2' />
       <FittingModelImage
-        src='https://cdn.thatzfit.com/default/models/slim-korean-male.png'
-        imageFileName='default-male-model'
+        src={currentFittingModel.modelUrl}
+        imageFileName={currentFittingModel.imageName}
         className='object-contain'
       />
       <ModelZoomDialog dialogTriggerClassName='absolute top-2 right-2' />
