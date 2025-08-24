db21cfc0db519fa07428fa74bf83f820fd49e4e4
lee0jae330 <xingxing2001@cau.ac.kr>
2025-08-24 21:51:27 +0900

    feat: 모델 사진 업로드 input 추가

---
+++ src/Features/FittingModel/Ui/FittingModelUploadInput/FittingModelUploadInput.tsx
@@ -0,0 +1,41 @@
+import type { ChangeEvent, RefObject } from 'react';
+
+import { useFittingModelStore } from '@/Entities/FittingModel';
+
+type FittingModelUploadInputProps = {
+  fileInputRef?: RefObject<HTMLInputElement>;
+  setIsModelAddDialogOpen: (isModelAddDialogOpen: boolean) => void;
+};
+
+export const FittingModelUploadInput = ({
+  fileInputRef,
+  setIsModelAddDialogOpen,
+}: FittingModelUploadInputProps) => {
+  const setAddedFittingModel = useFittingModelStore(
+    (state) => state.setAddedFittingModel,
+  );
+
+  const handleChangeFittingModel = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+
+    if (!file) {
+      return;
+    }
+    setAddedFittingModel({
+      modelImageFile: file,
+      modelImageUrl: URL.createObjectURL(file),
+    });
+    setIsModelAddDialogOpen(true);
+    event.target.value = '';
+  };
+
+  return (
+    <input
+      type='file'
+      ref={fileInputRef}
+      className='hidden'
+      accept='image/*'
+      onChange={handleChangeFittingModel}
+    />
+  );
+};
