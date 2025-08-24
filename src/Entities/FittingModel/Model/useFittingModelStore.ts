ad2897b62f12e72e57e022528583b68ccbc82217
lee0jae330 <xingxing2001@cau.ac.kr>
2025-08-24 21:26:47 +0900

    feat: 현재 선택된 모델, 추가할 모델 관련 전역 상태 정의

--- src/Entities/FittingModel/Model/useFittingModelStore.ts
+++ src/Entities/FittingModel/Model/useFittingModelStore.ts
@@ -3,10 +3,26 @@ import { devtools } from 'zustand/middleware';
 
 type FittingModelState = {
   defaultModels: Schema.FittingModel[];
+  currentFittingModel: {
+    modelUrl: string;
+    imageName: string;
+  };
+  addedFittingModel?: {
+    modelImageFile: File;
+    modelImageUrl: string;
+  };
 };
 
 type FittingModelAction = {
   setDefaultModels: (defaultModels: Schema.FittingModel[]) => void;
+  setCurrentFittingModel: (currentFittingModel: {
+    modelUrl: string;
+    imageName: string;
+  }) => void;
+  setAddedFittingModel: (addedFittingModel?: {
+    modelImageFile: File;
+    modelImageUrl: string;
+  }) => void;
 };
 
 type FittingModelStore = FittingModelState & FittingModelAction;
@@ -15,8 +31,19 @@ export const useFittingModelStore = create<FittingModelStore>()(
   devtools(
     (set) => ({
       defaultModels: [],
+      currentFittingModel: {
+        modelUrl: '',
+        imageName: '',
+      },
+      addedFittingModel: undefined,
       setDefaultModels: (defaultModels) => {
         set({ defaultModels }, undefined, 'setDefaultModels');
+      },
+      setCurrentFittingModel: (currentFittingModel) => {
+        set({ currentFittingModel }, undefined, 'setCurrentFittingModel');
+      },
+      setAddedFittingModel: (addedFittingModel) => {
+        set({ addedFittingModel }, undefined, 'setAddedFittingModel');
       },
     }),
     {
