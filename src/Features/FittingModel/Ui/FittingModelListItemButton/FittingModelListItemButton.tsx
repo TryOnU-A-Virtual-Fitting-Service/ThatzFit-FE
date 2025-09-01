2456657548d2203f9d87b39375633e2aaba27162
lee0jae330 <xingxing2001@cau.ac.kr>
2025-09-01 22:39:52 +0900

    feat: 각 피팅 모델을 선택할 수 있는 컴포넌트 추가

---
+++ src/Features/FittingModel/Ui/FittingModelListItemButton/FittingModelListItemButton.tsx
@@ -0,0 +1,46 @@
+import { useFittingModelStore } from '@/Entities/FittingModel';
+
+import { Button, DialogClose } from '@/Shared/Components';
+
+type FittingModelListItemButtonProps = {
+  fittingModel: Schema.FittingModel;
+};
+
+export const FittingModelListItemButton = ({
+  fittingModel,
+}: FittingModelListItemButtonProps) => {
+  const setCurrentFittingModel = useFittingModelStore(
+    (state) => state.setCurrentFittingModel,
+  );
+
+  const handleSelectFittingModel = ({
+    defaultModelUrl,
+    modelName,
+  }: {
+    defaultModelUrl: string;
+    modelName: Schema.FittingModel['modelName'];
+  }) => {
+    setCurrentFittingModel({
+      modelUrl: defaultModelUrl,
+      imageName: defaultModelUrl.split('/').pop() ?? '',
+      modelName,
+    });
+  };
+
+  return (
+    <DialogClose>
+      <Button
+        variant='ghost'
+        className='text-body1 text-grey-01 hover:bg-grey-07 hover:text-grey-01 w-full bg-white'
+        onClick={() =>
+          handleSelectFittingModel({
+            defaultModelUrl: fittingModel.defaultModelUrl,
+            modelName: fittingModel.modelName,
+          })
+        }
+      >
+        {fittingModel.modelName}
+      </Button>
+    </DialogClose>
+  );
+};
