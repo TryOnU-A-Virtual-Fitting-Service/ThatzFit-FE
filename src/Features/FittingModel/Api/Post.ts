4d9bfd1a985e1cfe624eb2ba247906fcbbbd9bcb
lee0jae330 <xingxing2001@cau.ac.kr>
2025-09-01 21:53:19 +0900

    feat: 모델 업로드 post api 추가 및 업로드 진행률 표시를 위한 xhr 객체 사용

--- src/Features/FittingModel/Api/Post.ts
+++ src/Features/FittingModel/Api/Post.ts
@@ -1,15 +1,58 @@
-import { post } from '@/Shared/Model';
+import { getUserToken } from '@/Entities/User';
 
-import type { PostFittingModelRequestDto } from '../Type';
+import { USER_TOKEN_KEY } from '@/Shared/Config';
+import type { SuccessResponse } from '@/Shared/Type';
 
-export const postFittingModel = async (dto: PostFittingModelRequestDto) => {
-  const formData = new FormData();
-  formData.append('file', dto.file);
+import type {
+  PostFittingModelRequestDto,
+  PostFittingModelResponseDto,
+} from '../Type';
 
-  await post('/api/default-model', {
-    body: formData,
-    headers: {
-      'Content-Type': undefined,
-    },
+export const postFittingModel = async ({
+  dto,
+  updateUploadProgress,
+}: {
+  dto: PostFittingModelRequestDto;
+  updateUploadProgress?: (uploadProgress: number) => void;
+}): Promise<SuccessResponse<PostFittingModelResponseDto>> => {
+  return new Promise((resolve, reject) => {
+    const formData = new FormData();
+    formData.append('file', dto.file);
+
+    // NOTE: upload progress 확인을 위해 xhr 사용
+    const xhr = new XMLHttpRequest();
+    let uploadProgress = 0;
+
+    const handleUploadProgress = (event: ProgressEvent) => {
+      if (event.lengthComputable) {
+        uploadProgress = (event.loaded / event.total) * 100;
+        updateUploadProgress?.(Math.floor(uploadProgress));
+      }
+    };
+
+    xhr.upload.addEventListener('progress', handleUploadProgress);
+
+    xhr.open('POST', '/api/default-model', true);
+    xhr.setRequestHeader(USER_TOKEN_KEY, getUserToken());
+    xhr.send(formData);
+
+    xhr.onload = () => {
+      if (xhr.status >= 200 && xhr.status <= 300) {
+        const result = JSON.parse(
+          xhr.response,
+        ) as SuccessResponse<PostFittingModelResponseDto>;
+        resolve(result);
+      } else {
+        reject(new Error(`${xhr.status} : ${xhr.statusText}`));
+      }
+    };
+
+    xhr.onerror = () => {
+      reject(new Error('Failed to upload file'));
+    };
+
+    xhr.onabort = () => {
+      reject(new Error('Upload aborted'));
+    };
   });
 };
