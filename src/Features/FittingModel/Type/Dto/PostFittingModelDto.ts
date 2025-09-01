c919cf27797b58faceb21e529d0bfe6d64bec32c
lee0jae330 <xingxing2001@cau.ac.kr>
2025-09-01 21:52:50 +0900

    feat: 모델 업로드 api dto 추가

--- src/Features/FittingModel/Type/Dto/PostFittingModelDto.ts
+++ src/Features/FittingModel/Type/Dto/PostFittingModelDto.ts
@@ -1,3 +1,10 @@
 export type PostFittingModelRequestDto = {
   file: File;
 };
+
+export type PostFittingModelResponseDto = {
+  id: Schema.FittingModel['defaultModelId'];
+  modelName: Schema.FittingModel['modelName'];
+  imageUrl: Schema.FittingModel['defaultModelUrl'];
+  sortOrder: Schema.FittingModel['sortOrder'];
+};
