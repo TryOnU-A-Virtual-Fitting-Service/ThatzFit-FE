1e51aa151cd7c51a8360c7b438815a9200f80ec9
lee0jae330 <xingxing2001@cau.ac.kr>
2025-09-01 21:55:58 +0900

    feat: 모델이 5개 이상일 경우 모델 업로드를 하지 못하도록 예외 처리

--- src/Features/FittingModel/Ui/FittingModelAddButton/FittingModelAddButton.tsx
+++ src/Features/FittingModel/Ui/FittingModelAddButton/FittingModelAddButton.tsx
@@ -2,15 +2,24 @@ import type { RefObject } from 'react';
 import { SquarePlus } from 'lucide-react';
 
 import { Button } from '@/Shared/Components';
+import { useToast } from '@/Shared/Model';
 
 type FittingModelAddButtonProps = {
+  fittingModelCount: number;
   fileInputRef?: RefObject<HTMLInputElement>;
 };
 
 export const FittingModelAddButton = ({
+  fittingModelCount,
   fileInputRef,
 }: FittingModelAddButtonProps) => {
+  const { toast } = useToast();
+
   const handleClickFittingModel = () => {
+    if (fittingModelCount >= 5) {
+      toast.error('최대 5개의 모델만 추가할 수 있어요.');
+      return;
+    }
     fileInputRef?.current?.click();
   };
 
