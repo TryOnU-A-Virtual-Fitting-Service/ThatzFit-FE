e6a088fb4ccafd3c9c8c2ab07757f9e0f7325e63
leeyeongjae <xingxing2001@cau.ac.kr>
2025-09-02 20:23:10 +0900

    feat: 피팅 실행 버튼 클릭 시 화면 캡처가 동작하도록 기능 추가

--- src/Features/Fitting/Ui/FittingButton/FittingButton.tsx
+++ src/Features/Fitting/Ui/FittingButton/FittingButton.tsx
@@ -1,14 +1,35 @@
+import { useShallow } from 'zustand/react/shallow';
+
+import { useFittingStore } from '@/Entities/Fitting';
+import { usePluginStore } from '@/Entities/Plugin';
+
 import { Button } from '@/Shared/Components';
 
-interface FittingButtonProps {
-  onClick?: () => void;
-}
+export const FittingButton = () => {
+  const setIsCapturing = useFittingStore((state) => state.setIsCapturing);
+  const { pluginWrapper, setIsPluginOpen } = usePluginStore(
+    useShallow((state) => ({
+      pluginWrapper: state.pluginWrapper,
+      setIsPluginOpen: state.setIsPluginOpen,
+    })),
+  );
 
-export const FittingButton = ({ onClick }: FittingButtonProps) => {
+  const handleClickFittingButton = () => {
+    setIsCapturing(true);
+    setIsPluginOpen(false);
+
+    if (!pluginWrapper) {
+      return;
+    }
+
+    pluginWrapper.classList.toggle('thatzfit-visible', false);
+    pluginWrapper.classList.toggle('thatzfit-hidden', true);
+  };
+
   return (
     <Button
       className='bg-grey-02 text-body1 hover:text-grey-07 h-8 w-full cursor-pointer rounded-md text-white hover:bg-black'
-      onClick={onClick}
+      onClick={handleClickFittingButton}
     >
       입어보기
     </Button>
