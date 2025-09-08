92af43d4cb061016acb5ef4d80e65bd154471aa1
lee0jae330 <xingxing2001@cau.ac.kr>
2025-09-08 14:53:32 +0900

    feat: 화면 캡처 관련 로직 추가

--- src/Widgets/Fitting/Ui/FittingClothingCaptureScreen/FittingClothingCaptureScreen.hook.ts
+++ src/Widgets/Fitting/Ui/FittingClothingCaptureScreen/FittingClothingCaptureScreen.hook.ts
@@ -2,8 +2,12 @@ import { type KeyboardEvent, type MouseEvent, useRef, useState } from 'react';
 import html2canvas from 'html2canvas-pro';
 import { useShallow } from 'zustand/react/shallow';
 
+import { usePostClothesImageDataUrl } from '@/Features/Fitting';
+
 import { useFittingStore } from '@/Entities/Fitting';
 import { usePluginStore } from '@/Entities/Plugin';
+
+import { useToast } from '@/Shared/Model';
 
 export const useCroppedClothing = () => {
   const { setIsCapturing, setCapturedClothingImage, setIsFittingDialogOpen } =
@@ -23,7 +27,8 @@ export const useCroppedClothing = () => {
     })),
   );
 
-  const { croppedImageToBlob } = useExtractCroppepClothing();
+  const { toast } = useToast();
+  const { croppedImageToBlob } = useExtractCroppedClothing();
   const [isDragging, setIsDragging] = useState<boolean>(false);
   const [isMouseMoving, setIsMouseMoving] = useState<boolean>(false);
   const [startX, setStartX] = useState<number>(0);
@@ -39,7 +44,11 @@ export const useCroppedClothing = () => {
   };
 
   const handleCroppedAreaMove = (event: MouseEvent) => {
-    if (!screenshotAreaRef.current || !screenshotBackgroundRef.current) {
+    if (
+      !screenshotAreaRef.current ||
+      !screenshotBackgroundRef.current ||
+      !isDragging
+    ) {
       return;
     }
     setIsMouseMoving(true);
@@ -50,7 +59,7 @@ export const useCroppedClothing = () => {
     const top = Math.min(y, startY);
     const left = Math.min(x, startX);
 
-    const right = window.innerWidth - Math.max(x, startX);
+    const right = document.body.clientWidth - Math.max(x, startX);
     const bottom = window.innerHeight - Math.max(y, startY);
 
     screenshotAreaRef.current.style.top = `${top}px`;
@@ -59,36 +68,46 @@ export const useCroppedClothing = () => {
     screenshotBackgroundRef.current.style.borderWidth = `${top}px ${right}px ${bottom}px ${left}px`;
   };
 
-  const handleCroppedEnd = (event: MouseEvent) => {
+  const handleCroppedEnd = async (event: MouseEvent) => {
     const x = event.clientX;
     const y = event.clientY;
 
+    const scrollWidth = window.innerWidth - document.body.clientWidth;
+
     const top = Math.min(y, startY);
     const left = Math.min(x, startX);
-    const width = Math.max(x, startX) - left;
+    const width = Math.max(x, startX) - left + scrollWidth;
     const height = Math.max(y, startY) - top;
 
-    croppedImageToBlob({
-      left,
-      top,
-      width,
-      height,
-      callback: setCapturedClothingImage,
-    });
     setIsFittingDialogOpen(true);
-
-    // NOTE: 캡처 완료 후 상태 초기화
     setIsDragging(false);
     setIsMouseMoving(false);
-    setIsCapturing(false);
-
-    if (!pluginWrapper) {
-      return;
+    try {
+      await croppedImageToBlob({
+        left,
+        top: top + window.scrollY,
+        width,
+        height,
+        callback: setCapturedClothingImage,
+      });
+    } catch {
+      toast.error('옷 캡처에 실패했어요.');
+      setIsFittingDialogOpen(false);
+      if (screenshotBackgroundRef.current) {
+        screenshotBackgroundRef.current.style.borderWidth = '0';
+      }
+      if (screenshotAreaRef.current) {
+        screenshotAreaRef.current.style.top = '0';
+        screenshotAreaRef.current.style.left = '0';
+      }
+    } finally {
+      setIsCapturing(false);
+      if (pluginWrapper) {
+        setIsPluginOpen(true);
+        pluginWrapper.classList.toggle('thatzfit-visible');
+        pluginWrapper.classList.toggle('thatzfit-hidden');
+      }
     }
-
-    setIsPluginOpen(true);
-    pluginWrapper.classList.toggle('thatzfit-visible');
-    pluginWrapper.classList.toggle('thatzfit-hidden');
   };
 
   const handleCancelCapture = (event: KeyboardEvent) => {
@@ -109,7 +128,13 @@ export const useCroppedClothing = () => {
   };
 };
 
-export const useExtractCroppepClothing = () => {
+export const useExtractCroppedClothing = () => {
+  const setIsImageProcessing = useFittingStore(
+    (state) => state.setIsImageProcessing,
+  );
+
+  const { mutateAsync: postClothesImageDataUrl } = usePostClothesImageDataUrl();
+
   const extractCroppedImageToBlob = (
     canvas?: HTMLCanvasElement,
     callback?: (blob: Blob) => void,
@@ -139,7 +164,46 @@ export const useExtractCroppepClothing = () => {
     height: number;
     callback: (blob: Blob) => void;
   }) =>
-    html2canvas(document.body).then((canvas) => {
+    html2canvas(document.body, {
+      allowTaint: true,
+      useCORS: true,
+      width: document.body.clientWidth,
+      onclone: async (cloneDoc) => {
+        setIsImageProcessing(true);
+        const imgList = cloneDoc.querySelectorAll('img');
+
+        const promiseList = [...imgList].map(async (img, idx) => {
+          const rect = img.getBoundingClientRect();
+          const isIntersecting = !(
+            left + width < rect.left ||
+            left > rect.right ||
+            top + height < rect.top ||
+            top > rect.bottom
+          );
+
+          // NOTE: 캡처 영역과 겹치는 이미지만 처리, 겹치지 않는 이미지는 cloneDoc에서 제거
+          if (isIntersecting) {
+            let imgUrl = '';
+            await postClothesImageDataUrl(
+              {
+                imageUrl: img.src,
+              },
+              {
+                onSuccess: ({ data: { dataUrl } }) => {
+                  imgUrl = dataUrl;
+                },
+              },
+            );
+            imgList[idx].src = imgUrl;
+          } else {
+            imgList[idx].src = '';
+            imgList[idx].style.display = 'none';
+          }
+        });
+        await Promise.all(promiseList);
+        setIsImageProcessing(false);
+      },
+    }).then((canvas) => {
       const img = canvas
         .getContext('2d')
         ?.getImageData(left, top, width, height);
