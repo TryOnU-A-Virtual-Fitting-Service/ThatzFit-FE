35241c0033956a66397325dcd6a547e6c6c8b8ce
lee0jae330 <xingxing2001@cau.ac.kr>
2025-09-08 14:42:53 +0900

    feat: 모델 업로드 버튼에서 모델 업로드를 실행하는 것 대신 mutation훅에서 useEffect로 요청하는 방식으로 변경

--- src/Features/FittingModel/Model/usePostFittingModel.ts
+++ src/Features/FittingModel/Model/usePostFittingModel.ts
@@ -1,16 +1,94 @@
-import { useMutation } from '@tanstack/react-query';
+import { useEffect } from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useShallow } from 'zustand/react/shallow';
+
+import {
+  fittingModelKeys,
+  useFittingModelStore,
+} from '@/Entities/FittingModel';
+import { usePluginStore } from '@/Entities/Plugin';
+
+import { isCustomError } from '@/Shared/Config';
+import { useToast } from '@/Shared/Model';
 
 import { postFittingModel } from '../Api';
 import type { PostFittingModelRequestDto } from '../Type';
 
+const UPLOAD_SUCCESS_MESSAGE = '새로운 모델을 추가했어요.';
+
 export const usePostFittingModel = () => {
-  return useMutation({
-    mutationFn: ({
-      dto,
-      updateUploadProgress,
-    }: {
-      dto: PostFittingModelRequestDto;
-      updateUploadProgress?: (uploadProgress: number) => void;
-    }) => postFittingModel({ dto, updateUploadProgress }),
+  const { toast } = useToast();
+  const queryClient = useQueryClient();
+
+  const {
+    isUploading,
+    setFittingModelUploadStatus,
+    addedFittingModel,
+    setCurrentFittingModel,
+  } = useFittingModelStore(
+    useShallow((state) => ({
+      isUploading: state.fittingModelUploadStatus.isUploading,
+      setFittingModelUploadStatus: state.setFittingModelUploadStatus,
+      addedFittingModel: state.addedFittingModel,
+      setCurrentFittingModel: state.setCurrentFittingModel,
+    })),
+  );
+
+  const pluginIframe = usePluginStore((state) => state.pluginIframe);
+
+  const { mutate: uploadFittingModel, isPending } = useMutation({
+    mutationFn: (dto: PostFittingModelRequestDto) => postFittingModel(dto),
   });
+
+  useEffect(() => {
+    if (isUploading && addedFittingModel) {
+      setTimeout(() => {
+        const iframeDocument = pluginIframe?.contentWindow?.document;
+        if (iframeDocument) {
+          iframeDocument.body.style.pointerEvents = 'auto';
+        }
+      }, 0);
+
+      uploadFittingModel(
+        {
+          file: addedFittingModel.modelImageFile,
+        },
+        {
+          onSuccess: ({ data }) => {
+            queryClient.invalidateQueries({
+              queryKey: fittingModelKeys.list(),
+            });
+            setCurrentFittingModel({
+              defaultModelUrl: data.imageUrl,
+              imageName: data.modelName,
+              modelName: data.modelName,
+              defaultModelId: data.id,
+            });
+            toast.success(UPLOAD_SUCCESS_MESSAGE);
+          },
+          onError: (error) => {
+            if (isCustomError(error) || error instanceof Error) {
+              toast.error(error.message);
+            }
+          },
+          onSettled: () => {
+            setFittingModelUploadStatus({
+              isUploading: false,
+            });
+          },
+        },
+      );
+    }
+  }, [
+    isUploading,
+    toast,
+    addedFittingModel,
+    queryClient,
+    pluginIframe,
+    setCurrentFittingModel,
+    setFittingModelUploadStatus,
+    uploadFittingModel,
+  ]);
+
+  return { isPending };
 };
--- src/Features/FittingModel/Ui/FittingModelUploadButton/FittingModelUploadButton.tsx
+++ src/Features/FittingModel/Ui/FittingModelUploadButton/FittingModelUploadButton.tsx
@@ -1,17 +1,8 @@
-import { useQueryClient } from '@tanstack/react-query';
 import { useShallow } from 'zustand/react/shallow';
 
-import { usePostFittingModel } from '@/Features/FittingModel';
-
-import {
-  fittingModelKeys,
-  useFittingModelStore,
-} from '@/Entities/FittingModel';
-import { usePluginStore } from '@/Entities/Plugin';
+import { useFittingModelStore } from '@/Entities/FittingModel';
 
 import { Button } from '@/Shared/Components';
-import { isCustomError } from '@/Shared/Config';
-import { useToast } from '@/Shared/Model';
 
 type FittingModelUploadButtonProps = {
   setIsFittingModelAddDialogOpen: (
@@ -24,25 +15,14 @@ export const FittingModelUploadButton = ({
   setIsFittingModelAddDialogOpen,
   setIsModelActionDialogOpen,
 }: FittingModelUploadButtonProps) => {
-  const queryClient = useQueryClient();
-
-  const pluginIframe = usePluginStore((state) => state.pluginIframe);
-
-  const { toast } = useToast();
-
-  const {
-    addedFittingModel,
-    setFittingModelUploadStatus,
-    setCurrentFittingModel,
-  } = useFittingModelStore(
-    useShallow((state) => ({
-      addedFittingModel: state.addedFittingModel,
-      setFittingModelUploadStatus: state.setFittingModelUploadStatus,
-      setCurrentFittingModel: state.setCurrentFittingModel,
-    })),
-  );
-
-  const { mutateAsync: postFittingModel } = usePostFittingModel();
+  const { addedFittingModel, setFittingModelUploadStatus } =
+    useFittingModelStore(
+      useShallow((state) => ({
+        addedFittingModel: state.addedFittingModel,
+        setFittingModelUploadStatus: state.setFittingModelUploadStatus,
+        setCurrentFittingModel: state.setCurrentFittingModel,
+      })),
+    );
 
   const handleClickUploadFittingModel = async () => {
     if (!addedFittingModel) {
@@ -50,48 +30,8 @@ export const FittingModelUploadButton = ({
     }
 
     setIsModelActionDialogOpen(false);
+    setFittingModelUploadStatus({ isUploading: true });
     setIsFittingModelAddDialogOpen(false);
-    setFittingModelUploadStatus({ isUploading: true, uploadProgress: 0 });
-    // NOTE: shadcn dialog 버그로 인해 body의 pointer-events 초기화 필요
-    setTimeout(() => {
-      const iframeDocument = pluginIframe?.contentWindow?.document;
-      if (iframeDocument) {
-        iframeDocument.body.style.pointerEvents = 'auto';
-      }
-    }, 0);
-
-    // NOTE: xhr 사용으로 인해 onSuccess 대신 try catch 사용
-    try {
-      const uploadResult = await postFittingModel({
-        dto: {
-          file: addedFittingModel.modelImageFile,
-        },
-        updateUploadProgress: (uploadProgress) => {
-          setFittingModelUploadStatus({ isUploading: true, uploadProgress });
-        },
-      });
-
-      queryClient.invalidateQueries({
-        queryKey: fittingModelKeys.list(),
-      });
-
-      setCurrentFittingModel({
-        modelUrl: uploadResult.data.imageUrl,
-        imageName: uploadResult.data.modelName,
-        modelName: uploadResult.data.modelName,
-      });
-
-      toast.success('새로운 모델을 추가했어요.');
-    } catch (error) {
-      if (isCustomError(error) || error instanceof Error) {
-        toast.error(error.message);
-      }
-    } finally {
-      setFittingModelUploadStatus({
-        isUploading: false,
-        uploadProgress: 0,
-      });
-    }
   };
 
   return (
