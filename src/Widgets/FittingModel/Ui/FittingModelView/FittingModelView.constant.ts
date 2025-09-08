8e3728748f6081490cdff8f69fea2b49ff945dc5
lee0jae330 <xingxing2001@cau.ac.kr>
2025-09-08 17:28:11 +0900

    feat: 모델 업로드, 가상 피팅 로딩뷰 컴포넌트 분리 및 불필요한 파일 삭제

--- src/Widgets/FittingModel/Ui/FittingModelView/FittingModelView.constant.ts
+++
@@ -1,4 +0,0 @@
-export const FITTING_MODEL_LOADING_MESSAGE = {
-  UPLOAD_MODEL: '모델이 될 준비중...',
-  EXECUTE_FITTING: '옷 갈아입는 중...',
-};
--- src/Widgets/FittingModel/Ui/FittingModelView/FittingModelView.tsx
+++ src/Widgets/FittingModel/Ui/FittingModelView/FittingModelView.tsx
@@ -1,7 +1,8 @@
 import { useEffect } from 'react';
 import { useShallow } from 'zustand/react/shallow';
 
-import { FITTING_MODEL_LOADING_MESSAGE } from '@/Widgets/FittingModel/Ui/FittingModelView/FittingModelView.constant';
+import { FittingLoadingView } from '@/Widgets/Fitting/Ui';
+import { FittingModelUploadLoadingView } from '@/Widgets/FittingModel/Ui/FittingModelUploadLoadingView';
 
 import { usePostFitting } from '@/Features/Fitting/Model';
 import { usePostFittingModel } from '@/Features/FittingModel';
@@ -13,8 +14,6 @@ import {
 
 import { FittingModelActionDialog } from '../FittingModelActionDialog';
 import { ModelZoomDialog } from '../ModelZoomDialog';
-
-import { FittingModelViewLoading } from './FittingModelView.loading';
 
 export const FittingModelView = () => {
   const { currentFittingModel, fittingModelList, setCurrentFittingModel } =
@@ -45,29 +44,20 @@ export const FittingModelView = () => {
     }
   }, [fittingModelList, currentFittingModel, setCurrentFittingModel]);
 
-  const loadingText = (() => {
-    if (isPostFittingModelPending) {
-      return FITTING_MODEL_LOADING_MESSAGE.UPLOAD_MODEL;
-    }
-    if (isPostFittingPending) {
-      return FITTING_MODEL_LOADING_MESSAGE.EXECUTE_FITTING;
-    }
-    return '';
-  })();
-
   return (
     <div className='bg-grey-08 relative flex h-[18.75rem] w-full justify-center rounded-[0.375rem] p-[0.5625rem] select-none'>
       <FittingModelActionDialog dialogTriggerClassName='absolute top-2 left-2' />
-      {isPostFittingModelPending || isPostFittingPending ? (
-        <FittingModelViewLoading loadingText={loadingText} />
-      ) : (
+      {isPostFittingPending && (
+        <FittingLoadingView isLoading={isPostFittingPending} />
+      )}
+      {isPostFittingModelPending && <FittingModelUploadLoadingView />}
+      {!isPostFittingPending && !isPostFittingModelPending && (
         <FittingModelImage
           src={currentFittingModel.defaultModelUrl}
           imageFileName={currentFittingModel.imageName}
           className='object-contain'
         />
       )}
-
       <ModelZoomDialog dialogTriggerClassName='absolute top-2 right-2' />
     </div>
   );
