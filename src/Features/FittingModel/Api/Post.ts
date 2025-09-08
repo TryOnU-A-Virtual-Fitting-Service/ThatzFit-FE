a93d44b3248db0d702f7a531472fa279444c0cbb
lee0jae330 <xingxing2001@cau.ac.kr>
2025-09-08 14:40:13 +0900

    feat: xhr 객체 사용 대신 ky로 교체
    - api 요청 업로드율이 필요하지 않음.

--- src/Features/FittingModel/Api/Post.ts
+++ src/Features/FittingModel/Api/Post.ts
@@ -1,58 +1,16 @@
-import { getUserToken } from '@/Entities/User';
-
-import { USER_TOKEN_KEY } from '@/Shared/Config';
-import type { SuccessResponse } from '@/Shared/Type';
+import { post } from '@/Shared/Model';
 
 import type {
   PostFittingModelRequestDto,
   PostFittingModelResponseDto,
 } from '../Type';
 
-export const postFittingModel = async ({
-  dto,
-  updateUploadProgress,
-}: {
-  dto: PostFittingModelRequestDto;
-  updateUploadProgress?: (uploadProgress: number) => void;
-}): Promise<SuccessResponse<PostFittingModelResponseDto>> => {
-  return new Promise((resolve, reject) => {
-    const formData = new FormData();
-    formData.append('file', dto.file);
+export const postFittingModel = async (dto: PostFittingModelRequestDto) => {
+  const formData = new FormData();
+  formData.append('file', dto.file);
 
-    // NOTE: upload progress 확인을 위해 xhr 사용
-    const xhr = new XMLHttpRequest();
-    let uploadProgress = 0;
-
-    const handleUploadProgress = (event: ProgressEvent) => {
-      if (event.lengthComputable) {
-        uploadProgress = (event.loaded / event.total) * 100;
-        updateUploadProgress?.(Math.floor(uploadProgress));
-      }
-    };
-
-    xhr.upload.addEventListener('progress', handleUploadProgress);
-
-    xhr.open('POST', '/api/default-model', true);
-    xhr.setRequestHeader(USER_TOKEN_KEY, getUserToken());
-    xhr.send(formData);
-
-    xhr.onload = () => {
-      if (xhr.status >= 200 && xhr.status <= 300) {
-        const result = JSON.parse(
-          xhr.response,
-        ) as SuccessResponse<PostFittingModelResponseDto>;
-        resolve(result);
-      } else {
-        reject(new Error(`${xhr.status} : ${xhr.statusText}`));
-      }
-    };
-
-    xhr.onerror = () => {
-      reject(new Error('Failed to upload file'));
-    };
-
-    xhr.onabort = () => {
-      reject(new Error('Upload aborted'));
-    };
-  });
+  return post<PostFittingModelResponseDto>(`/api/default-model`, {
+    headers: undefined,
+    body: formData,
+  }).then((res) => res.json());
 };
