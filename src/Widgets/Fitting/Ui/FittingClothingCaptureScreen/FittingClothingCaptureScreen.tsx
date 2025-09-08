14cade0e757e7316216f2f4bea0365518d055b5b
lee0jae330 <xingxing2001@cau.ac.kr>
2025-09-08 14:52:53 +0900

    feat: 피팅을 위한 화면 캡처 컴포넌트 구현

--- src/Widgets/Fitting/Ui/FittingClothingCaptureScreen/FittingClothingCaptureScreen.tsx
+++ src/Widgets/Fitting/Ui/FittingClothingCaptureScreen/FittingClothingCaptureScreen.tsx
@@ -11,9 +11,11 @@ export const FittingClothingCaptureScreen = () => {
   const isCapturing = useFittingStore((state) => state.isCapturing);
 
   const pluginEntryWrapper = usePluginEntryStore((state) => state.entryWrapper);
+  const isFittingDialogOpen = useFittingStore(
+    (state) => state.isFittingDialogOpen,
+  );
   const {
     isDragging,
-    isMouseMoving,
     screenshotBackgroundRef,
     screenshotAreaRef,
     handleCroppedStart,
@@ -31,26 +33,29 @@ export const FittingClothingCaptureScreen = () => {
       tabIndex={-1}
       onKeyDown={handleCancelCapture}
       ref={(element) => {
-        element?.focus();
+        if (element) {
+          element.focus({ preventScroll: true });
+        }
       }}
+      className={cn(isFittingDialogOpen && 'hidden')}
     >
       <div
         className={cn(
-          'fixed top-0 left-0 z-[10000] h-screen w-screen border-solid border-black opacity-30',
-          isMouseMoving ? 'bg-none' : 'bg-black',
-          isDragging && 'cursor-crosshair',
+          'fixed top-0 left-0 z-[10000] block h-full w-full border-solid border-black opacity-30',
+          isCapturing && 'cursor-crosshair',
         )}
+        style={{
+          borderWidth: isCapturing ? `0 0 ${window.innerHeight}px 0` : 0,
+        }}
         onMouseDown={handleCroppedStart}
         onMouseMove={handleCroppedAreaMove}
         onMouseUp={handleCroppedEnd}
         ref={screenshotBackgroundRef}
       >
-        {isDragging && (
-          <div
-            className='fixed z-[10001] h-full w-full before:fixed before:top-[-100%] before:left-[-100%] before:h-full before:w-full before:border-r-[1px] before:border-b-[1px] before:border-white before:content-[""] after:fixed after:top-[0] after:left-[0] after:h-full after:w-full after:border-t-[1px] after:border-l-[1px] after:content-[""]'
-            ref={screenshotAreaRef}
-          ></div>
-        )}
+        <div
+          className="fixed z-[10001] h-full w-full border-r-[1px] border-b-[1px] before:absolute before:top-[-100%] before:left-[-100%] before:border-solid before:border-red-500 before:content-['']"
+          ref={screenshotAreaRef}
+        ></div>
       </div>
       {!isDragging && (
         <div className='fixed top-20 left-1/2 z-[10000] w-2xl -translate-x-1/2 rounded-lg bg-white py-3 text-center'>
