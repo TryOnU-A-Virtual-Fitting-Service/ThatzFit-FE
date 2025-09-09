279e3541033077d8401eb03a08d8c825baf4c277
lee0jae330 <xingxing2001@cau.ac.kr>
2025-09-10 03:19:04 +0900

    feat: fitting history 관련 UI 추가

--- src/Widgets/FittingHistory/Ui/FittingHistorySection/FittingHistorySection.tsx
+++ src/Widgets/FittingHistory/Ui/FittingHistorySection/FittingHistorySection.tsx
@@ -1,17 +1,39 @@
+import { useQuery } from '@tanstack/react-query';
+
 import { FittingHistoryInfoTooltip } from '@/Features/FittingHistory';
 
+import { fittingHistoryQueries } from '@/Entities/FittingHistory';
+
+import {
+  FittingHistoryList,
+  FittingHistoryListEmptyView,
+} from '../FittingHistoryList';
+
 export const FittingHistorySection = () => {
+  const {
+    data: fittingHistoryList,
+    isLoading,
+    isSuccess,
+  } = useQuery({
+    ...fittingHistoryQueries.listOptions(),
+    select: (response) => response.data,
+  });
+
   return (
-    <section className='mt-4 mb-[0.875rem] flex flex-col gap-1'>
+    <section className='mt-4 mb-[0.875rem] flex w-full flex-col gap-1 select-none'>
       <div className='flex w-full items-center justify-between'>
         <span className='text-body1'>피팅 히스토리</span>
         <FittingHistoryInfoTooltip />
       </div>
-      <div className='bg-grey-08 mt-2 flex h-[3.625rem] w-full items-center rounded-md'>
-        <span className='text-body3-regular text-grey-04 w-full text-center'>
-          입어본 옷이 없어요. 새로운 옷을 입어볼까요?
-        </span>
-      </div>
+      {fittingHistoryList?.tryOnResults.length === 0 ? (
+        <FittingHistoryListEmptyView />
+      ) : (
+        <FittingHistoryList
+          fittingHistoryList={fittingHistoryList?.tryOnResults}
+          isLoading={isLoading}
+          isSuccess={isSuccess}
+        />
+      )}
     </section>
   );
 };
