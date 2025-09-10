2927085340375cfe7617907aa93abd16e9573efe
lee0jae330 <xingxing2001@cau.ac.kr>
2025-09-10 20:49:06 +0900

    feat: fitting history item 클릭 시 모델이 변경되도록 기능 추가

--- src/Features/FittingHistory/Ui/FittingHistoryListItem/FittingHistoryListItem.tsx
+++ src/Features/FittingHistory/Ui/FittingHistoryListItem/FittingHistoryListItem.tsx
@@ -1,3 +1,5 @@
+import { useFittingModelStore } from '@/Entities/FittingModel';
+
 import { Button } from '@/Shared/Components';
 
 type FittingHistoryListItemProps = {
@@ -7,10 +9,25 @@ type FittingHistoryListItemProps = {
 export const FittingHistoryListItem = ({
   fittingHistory,
 }: FittingHistoryListItemProps) => {
+  const setCurrentFittingModel = useFittingModelStore(
+    (state) => state.setCurrentFittingModel,
+  );
+
+  const handleClickFittingHistoryItem = () => {
+    const { tryOnResultUrl, modelName, defaultModelId } = fittingHistory;
+    setCurrentFittingModel({
+      modelName,
+      defaultModelId,
+      defaultModelUrl: tryOnResultUrl,
+      imageName: tryOnResultUrl,
+    });
+  };
+
   return (
     <Button
       variant='ghost'
       className='border-grey-04 h-10 w-10 min-w-0 shrink-0 cursor-pointer rounded-md border bg-white p-0 hover:bg-white'
+      onClick={handleClickFittingHistoryItem}
     >
       <img
         src={fittingHistory.tryOnResultUrl}
