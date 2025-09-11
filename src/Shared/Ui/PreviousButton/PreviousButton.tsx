3b130c7a4610f886c1bcbdbd5397c1a4d0f0fc99
leeyeongjae <xingxing2001@cau.ac.kr>
2025-09-11 19:25:10 +0900

    feat: 이전 버튼 추가

---
+++ src/Shared/Ui/PreviousButton/PreviousButton.tsx
@@ -0,0 +1,35 @@
+import type { ComponentProps } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ChevronRight } from 'lucide-react';
+
+import { Button } from '@/Shared/Components';
+import { cn } from '@/Shared/Lib';
+
+type PreviousButtonProps = {
+  variant?: ComponentProps<typeof Button>['variant'];
+  size?: ComponentProps<typeof Button>['size'];
+  className?: string;
+};
+
+export const PreviousButton = ({
+  variant,
+  size,
+  className,
+}: PreviousButtonProps) => {
+  const navigate = useNavigate();
+
+  const handleClickPreviousButton = () => {
+    navigate('..');
+  };
+
+  return (
+    <Button
+      variant={variant ?? 'ghost'}
+      size={size ?? 'icon'}
+      onClick={handleClickPreviousButton}
+      className={cn('cursor-pointer', className)}
+    >
+      <ChevronRight />
+    </Button>
+  );
+};
