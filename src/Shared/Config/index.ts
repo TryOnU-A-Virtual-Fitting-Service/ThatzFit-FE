e6abf51df902df6301a3def2979abfdc1ac9c6b3
leeyeongjae <xingxing2001@cau.ac.kr>
2025-11-22 17:33:13 +0900

    chore: 배럴 파일 추가

--- src/Shared/Config/index.ts
+++ src/Shared/Config/index.ts
@@ -1,4 +1,5 @@
 export { BASE_URL } from './Api';
 export { createCustomError, CustomError, isCustomError } from './CustomError';
 export { queryClient } from './QueryClient';
+export { TAILWIND_CSS_CDN, TAILWIND_CSS_VARIABLE } from './TailwindCssVariable';
 export { USER_TOKEN_KEY } from './Token';
