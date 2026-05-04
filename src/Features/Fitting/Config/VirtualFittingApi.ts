c0274370c5423f1a53347f8a11bfa6b2566de064
givemethatsewon <nvpz1598@gmail.com>
2026-05-04 14:28:41 +0900

    fix: re-enable virtual fitting api

--- src/Features/Fitting/Config/VirtualFittingApi.ts
+++ src/Features/Fitting/Config/VirtualFittingApi.ts
@@ -1,3 +1,3 @@
-export const IS_VIRTUAL_FITTING_API_DISABLED = true;
+export const IS_VIRTUAL_FITTING_API_DISABLED = false;
 
 export const VIRTUAL_FITTING_API_DISABLED_MESSAGE = '피팅 API는 잠시 꺼뒀어요.';
